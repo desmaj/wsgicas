4489bc392e5f25cee9ff852d204604c3aa04658d
Matthew Desmarais <matthew.desmarais@gmail.com>
2016-01-09 16:57:23 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='wsgicas',
+      version=version,
+      description="WSGI CAS (Central Authentication Service) Utilities",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='cas wsgi',
+      author='Matthew J Desmarais',
+      author_email='matthew.desmarais@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
